056d7e221741951e04238a3723e26e6169ec85ea
Elias <elre233@gmail.com>
2020-09-14 20:06:49 -0400

    starting rhythm guide

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,11 @@
 use std::fs::File;
 use std::io::prelude::*;
 const BEAT_WIDTH: usize = 2;
-const EMPTY: char = '-';
+const EMPTY_NOTE: &str = "--";
 ///TODO: Should be configurable
 const BEATS_PER_MEASURE: usize = 16;
-const MEAURE_SEPARATOR: char = '|';
-const BEAT_SEPARATOR: char = ' ';
+const MEASURE_SEPARATOR: char = '|';
+const NOTE_SEPARATOR: char = ' ';
 
 pub fn generate_template() -> std::io::Result<()>{
   ///TODO: take in path as arg, use it to create file with specific location
@@ -21,22 +21,25 @@ pub fn generate_template() -> std::io::Result<()>{
   for _ in 0..num_rows {
     //Before write the strings, write a some pipes to help show where the quarter note beats are
 
-    // for _ in 0..BEAT
+    for _ in 0..BEATS_PER_MEASURE * measures_per_row {
+      //offset from the string note and first measure separator
+      let mut rhythm_guide = String::from("  ");
+
+    }
 
     //For each string
     for open_note in standard_tuning.iter() {
       let mut line = String::new();
       line.push(*open_note);
+      line.push(MEASURE_SEPARATOR);
       for measure in 0..measures_per_row {
-        line.push(MEAURE_SEPARATOR);
         for _ in 0..BEATS_PER_MEASURE {
-          for _ in 0..BEAT_WIDTH {
-            line.push(EMPTY);
-          }
-          line.push(BEAT_SEPARATOR);
+          line.push_str(EMPTY_NOTE);
+          line.push(NOTE_SEPARATOR);
         }
+        line.pop();
+        line.push(MEASURE_SEPARATOR);
       }
-      line.push(MEAURE_SEPARATOR);
       line.push('\n');
       file.write_all(line.as_bytes());
     }
