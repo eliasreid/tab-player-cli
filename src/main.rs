8fc6d2b2569e6c652f2f566c10b497411a1ae215
Elias Reid <ereid@omnitechelectronics.ca>
2020-09-15 00:59:29 -0300

    parse one row

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,36 @@ use std::time::Duration;
 use clap::{App, Arg};
 
 mod parser;
-use parser::generate_template;
 
 fn main() {
 
   let args = App::new("tab-synth")
     .version("0.1")
-    .about("WIP - play tabs from cli")
+    .about("WIP - play tabs from cli eventually..")
     .arg(Arg::with_name("generate-template")
            .short("g")
            .long("generate-template")
            .value_name("FILE")
            .takes_value(true)
            .default_value("./tab-template.txt")
-           .required(true))
+           .required(false))
+    .arg(Arg::with_name("play-file")
+           .short("p")
+           .long("play")
+           .value_name("FILE")
+           .takes_value(true)
+           .required(false))
     .get_matches();
 
   //If --generate-template, then generate a template
 
   if args.is_present("generate-template") {
     let path = args.value_of("generate-template").unwrap();
-    generate_template(path);
+    parser::generate_template(path);
     return;
+  } else if args.is_present("play-file") {
+    let file = args.value_of("play-file").unwrap();
+    parser::play_file(file);
   }
 
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,20 @@
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-const BEAT_WIDTH: usize = 2;
+
+use itertools::Itertools;
+
+const BEAT_WIDTH: u32 = 3;
+const BEAT_OFFSET: u32 = 2;
 const EMPTY_NOTE: &str = "--";
 ///TODO: Should be configurable
 const BEATS_PER_MEASURE: usize = 16;
 const MEASURE_SEPARATOR: char = '|';
 const NOTE_SEPARATOR: char = ' ';
+const HOLD_IDEN: &str = ">>";
 
 pub fn generate_template(save_file: &str) -> std::io::Result<()>{
-  ///TODO: take in path as arg, use it to create file with specific location
+  //!TODO: take in path as arg, use it to create file with specific location
   let mut file = File::create(save_file)?;
 
   ///TODO: configurable - should annotate if standard tuning?
@@ -21,22 +27,22 @@ pub fn generate_template(save_file: &str) -> std::io::Result<()>{
   //Before anything, write some times steps to help guide
   let mut time_fractions = String::from(" ");
   //TODO: unhard code this - should be based on timing parameters.
-  file.write_all(b" 1/4         2/4         3/4         4/4\n");
+  file.write_all(b" 1/4         2/4         3/4         4/4\n")?;
   
   for _ in 0..num_rows {
     //TODO: unhard code this - should be based on timing parameters.
-    file.write_all(b"  |           |           |           |         ");
+    file.write_all(b"  |           |           |           |         ")?;
     for _ in 0..(measures_per_row - 1) { 
-      file.write_all(b"  |           |           |           |");
+      file.write_all(b"  |           |           |           |")?;
     }
-    file.write_all(b"\n");
+    file.write_all(b"\n")?;
 
     //For each string
     for open_note in standard_tuning.iter() {
       let mut line = String::new();
       line.push(*open_note);
       line.push(MEASURE_SEPARATOR);
-      for measure in 0..measures_per_row {
+      for _ in 0..measures_per_row {
         for _ in 0..BEATS_PER_MEASURE {
           line.push_str(EMPTY_NOTE);
           line.push(NOTE_SEPARATOR);
@@ -45,10 +51,129 @@ pub fn generate_template(save_file: &str) -> std::io::Result<()>{
         line.push(MEASURE_SEPARATOR);
       }
       line.push('\n');
-      file.write_all(line.as_bytes());
+      file.write_all(line.as_bytes())?;
     }
-    file.write_all(b"\n");
+    file.write_all(b"\n")?;
   }
 
   Ok(())
+}
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum StringLetter {
+  e,
+  B,
+  G,
+  D,
+  A,
+  E
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct Note {
+  start_beat: u32, //Rhythm wise, where does the note start - units of sixteenth note.
+  fret: u32,
+  length: u32,
+  string: StringLetter
+}
+
+impl Note {
+  fn new(start_beat: u32, fret: u32, length: u32, string: StringLetter) -> Note {
+    Note{
+      start_beat: start_beat,
+      fret: fret,
+      length: length,
+      string: string
+    }
+  }
+}
+
+pub fn play_file(play_file: &str) -> std::io::Result<()> {
+  //First need to parse file into a format from which the notes can be applied to the samples
+  // that is, a vec of {fret, start_beat, length} for each string
+  let notes = parse_file(play_file)?;
+  Ok(())
+}
+
+fn parse_file(file: &str) -> Result<Vec<Note>, std::io::Error> {
+  let contents = fs::read_to_string(file)?;
+
+  let mut parsed_notes: Vec<Note> =  Vec::new();
+
+  for line in contents.lines() {
+    //Check first char of line
+
+    println!("parsing line: {}", line);
+
+    let letter = match line.chars().nth(0) {
+      Some(c) => match c {
+        'e' => Some(StringLetter::e),
+        'B' => Some(StringLetter::B),
+        'G' => Some(StringLetter::G),
+        'D' => Some(StringLetter::D),
+        'A' => Some(StringLetter::A),
+        'E' => Some(StringLetter::E),
+        _ => None
+        },
+      None => None
+    };
+    let mut note_active = false;
+    let mut read_fret: u32 = 0;
+    let mut note_start: u32 = 0;
+    let mut note_length: u32 = 1;
+
+    if let Some(letter) = letter {
+      //Found letter identifer at start of line - continue to parse line for notes
+
+      //Seems to work, but don't understand well, not sure about how many copies are being made.
+      for (i, s) in line.chars().skip(BEAT_OFFSET as usize).chunks(3).into_iter().enumerate() {
+        //this gives the 3 char string that we can check for note
+        let val: String = s.collect();
+        println!("{}", val);
+        if let Ok(fret) = val.trim().parse::<u32>() {
+
+          if note_active {
+            parsed_notes.push(Note::new(note_start, read_fret, note_length, letter));
+            note_length = 1;
+            note_active = false;
+          }
+          //fret found! add note
+          note_active = true;
+          read_fret = fret;
+          //TODO: add offset to i for row
+          note_start = i as u32;
+
+          println!("index {}, fret {}", i, fret);
+        } else if note_active && val.contains(HOLD_IDEN) {
+          note_length += 1;
+          //Don't need to do anything? 
+        } else if note_active && val.contains(EMPTY_NOTE){
+          parsed_notes.push(Note::new(note_start, read_fret, note_length, letter));
+          note_length = 1;
+          note_active = false;
+        }
+      }
+    }
+    
+  }
+
+  Ok(parsed_notes)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn parser_test() {
+    let correct_notes = vec![
+      Note::new(4, 2, 1, StringLetter::G),
+      Note::new(0, 7, 6, StringLetter::A),
+      Note::new(6, 7, 2, StringLetter::A),
+      Note::new(8, 10, 3, StringLetter::A),
+      Note::new(11, 7, 3, StringLetter::A),
+      Note::new(14, 5, 2, StringLetter::A),
+    ];
+    
+    let notes = parse_file("./test/seven-nation-army.txt").unwrap();
+    assert_eq!(notes, correct_notes);
+  }
 }
